1f6239bf0159dbea12ca5d2cbd4a4d7a7452d5e2
nox <ijmermet@gmail.com>
2020-02-02 17:35:06 -0300

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+from setuptools import setup
+
+setup(
+    name='babikes',
+    version='0.0.1',
+    description='',
+    url='https://github.com/crossnox/YouConfigMe',
+    author='CrossNox',
+    install_requires=[],
+    extras_require={
+        'dev': ['precommit', 'pytest', 'mypy', 'flake8', 'isort', 'black', 'pylint'],
+    },
+    packages=['youconfigme'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
