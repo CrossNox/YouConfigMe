d0fd891f299d4e07d626235fae18a9cca79f2394
CrossNox <ijmermet@gmail.com>
2020-10-11 23:04:51 -0300

    touch .nojekyll file

--- youconfigme/__init__.py
+++ youconfigme/__init__.py
@@ -1,5 +1,5 @@
 """Entrypoint to make relevant classes available at the top level."""
 from .youconfigme import AutoConfig, Config, ConfigItemNotFound, ConfigSection
 
-__version__ = '0.6.11'
+__version__ = '0.6.12'
 __all__ = ['AutoConfig', 'Config', 'ConfigItemNotFound', 'ConfigSection']
