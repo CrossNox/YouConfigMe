fc9175f3d03586d9ea11500b22b08ee9ed6c4deb
CrossNox <ijmermet@gmail.com>
2022-06-04 18:53:18 -0300

    fix version

--- youconfigme/__init__.py
+++ youconfigme/__init__.py
@@ -1,5 +1,5 @@
 """Entrypoint to make relevant classes available at the top level."""
 from .youconfigme import AutoConfig, Config, ConfigItemNotFound, ConfigSection
 
-__version__ = '0.7.15'
-__all__ = ['AutoConfig', 'Config', 'ConfigItemNotFound', 'ConfigSection']
+__version__ = "0.7.0"
+__all__ = ["AutoConfig", "Config", "ConfigItemNotFound", "ConfigSection"]
