4196364526108967fc07c6c5f10d480ca9eaff74
CrossNox <ijmermet@gmail.com>
2022-06-04 23:37:38 -0300

    patch subprocess.run

--- tests/test_pass.py
+++ tests/test_pass.py
@@ -3,46 +3,49 @@
 # pylint: disable=missing-function-docstring
 
 import os
-import subprocess
+from unittest.mock import patch
 
 import pytest
 from youconfigme import Config
 
+PASS = "oijfoijsdfjoifoij"
+
 
 @pytest.fixture
 def config_underscore():
-    subprocess.run(["pass", "generate", "--force", "ycm_test"], check=True)
-    pwd = (
-        subprocess.run(["pass", "ycm_test"], capture_output=True, check=True)
-        .stdout.decode()
-        .strip()
-    )
     os.environ["A_K1"] = "ycm_test"
     os.environ["K4"] = "ycm_test"
-    yield pwd, Config(
+    yield Config(
         from_items={"a": {"k1": 1, "k2": "ycm_test"}, "b": {"k3": "ycm_test", "k4": 4}},
         default_section="b",
     )
     del os.environ["A_K1"]
     del os.environ["K4"]
-    subprocess.run(["pass", "rm", "--force", "ycm_test"], check=True)
 
 
-def test_k1(config_underscore):
-    pwd, cfg = config_underscore
-    assert cfg.a.k1(from_pass=True) == pwd
+@patch("youconfigme.getpass.subprocess.run")
+def test_k1(mock_run, config_underscore):
+    mock_run.return_value.stdout.decode.return_value.strip.return_value = PASS
+    cfg = config_underscore
+    assert cfg.a.k1(from_pass=True) == PASS
 
 
-def test_k2(config_underscore):
-    pwd, cfg = config_underscore
-    assert cfg.a.k2(from_pass=True) == pwd
+@patch("youconfigme.getpass.subprocess.run")
+def test_k2(mock_run, config_underscore):
+    mock_run.return_value.stdout.decode.return_value.strip.return_value = PASS
+    cfg = config_underscore
+    assert cfg.a.k2(from_pass=True) == PASS
 
 
-def test_k3(config_underscore):
-    pwd, cfg = config_underscore
-    assert cfg.k3(from_pass=True) == pwd
+@patch("youconfigme.getpass.subprocess.run")
+def test_k3(mock_run, config_underscore):
+    mock_run.return_value.stdout.decode.return_value.strip.return_value = PASS
+    cfg = config_underscore
+    assert cfg.k3(from_pass=True) == PASS
 
 
-def test_k4(config_underscore):
-    pwd, cfg = config_underscore
-    assert cfg.k4(from_pass=True) == pwd
+@patch("youconfigme.getpass.subprocess.run")
+def test_k4(mock_run, config_underscore):
+    mock_run.return_value.stdout.decode.return_value.strip.return_value = PASS
+    cfg = config_underscore
+    assert cfg.k4(from_pass=True) == PASS
