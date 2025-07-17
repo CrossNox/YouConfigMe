3f697d07eb88587128c0bd696bc416934d0060a0
CrossNox <ijmermet@gmail.com>
2025-07-17 12:53:34 -0300

    commit

--- youconfigme/__init__.py
+++ youconfigme/__init__.py
@@ -1,15 +1,6 @@
 """Entrypoint to make relevant classes available at the top level."""
 
-from typing import TYPE_CHECKING
-
 from .youconfigme import AutoConfig, Config, ConfigItemNotFound, ConfigSection
-
-if TYPE_CHECKING:
-    # Re-export for type checkers
-    from .youconfigme import AutoConfig as AutoConfig
-    from .youconfigme import Config as Config
-    from .youconfigme import ConfigItemNotFound as ConfigItemNotFound
-    from .youconfigme import ConfigSection as ConfigSection
 
 __version__ = "1.0.0"
 __all__ = ["AutoConfig", "Config", "ConfigItemNotFound", "ConfigSection"]
