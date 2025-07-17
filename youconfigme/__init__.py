527cd4707e9fff287ae7e8f04205a250ef971112
CrossNox <ijmermet@gmail.com>
2025-07-17 16:30:14 -0300

    commit

--- youconfigme/__init__.py
+++ youconfigme/__init__.py
@@ -2,5 +2,5 @@
 
 from .youconfigme import AutoConfig, Config, ConfigItemNotFound, ConfigSection
 
-__version__ = "1.1.0"
+__version__ = "2.0.0"
 __all__ = ["AutoConfig", "Config", "ConfigItemNotFound", "ConfigSection"]
